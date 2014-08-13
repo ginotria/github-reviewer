7002b7cc17ea0418f5a5458fb8dd7257408e6e48
gino <romanangelo.tria@gmail.com>
2014-08-14 07:54:40 +0800

    added to form to get github token and repo

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,4 +1,20 @@
 
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
 String.prototype.trunc =
      function(n,useWordBoundary){
          var toLong = this.length>n,
